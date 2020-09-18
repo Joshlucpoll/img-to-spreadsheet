63c642cfcf7c6d5d0b10348853581b00652629ea
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-09-18 22:00:22 +0100

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+echo 'img-to-spreadsheet.joshlucpoll.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:joshlucpoll/img-to-spreadsheet.git master:gh-pages
+cd -
