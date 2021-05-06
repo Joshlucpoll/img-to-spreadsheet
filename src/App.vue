66ec58d91b0be11f6e2a2f1fc755d644d19cc680
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2021-05-06 12:29:52 +0100

    Cleaned up and added subpixel spreadsheet version

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,114 @@
 <template>
   <div id="app">
     <div class="title">Image to Spreadsheet Converter</div>
-    <div id="file-input-container">
-      <input id="file" type="file" accept="image/*" @change="onFileChange($event)"/> 
-      <label class="file-label button" for="file">
-        <svg width="1em" height="1em" viewBox="0 0 16 16" id="file-icon" class="bi bi-file-earmark-image" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M12 16a2 2 0 0 0 2-2V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8zM3 2a1 1 0 0 1 1-1h5.5v2A1.5 1.5 0 0 0 11 4.5h2V10l-2.083-2.083a.5.5 0 0 0-.76.063L8 11 5.835 9.7a.5.5 0 0 0-.611.076L3 12V2z"/>
-          <path fill-rule="evenodd" d="M6.502 7a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-        </svg>
-        {{ fileText }}
-      </label>
-      <div class="button convert" v-if="fileText !== 'Feed me images...'" @click="onFileUpload()"> 
-        Convert! 
-        <div v-if="converting" class="loader"></div>
+    <div class="col">
+      <div id="file-input-container">
+        <input
+          id="file"
+          type="file"
+          accept="image/*"
+          @change="onFileChange($event)"
+        />
+        <label class="file-label button" for="file">
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            id="file-icon"
+            class="bi bi-file-earmark-image"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12 16a2 2 0 0 0 2-2V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8zM3 2a1 1 0 0 1 1-1h5.5v2A1.5 1.5 0 0 0 11 4.5h2V10l-2.083-2.083a.5.5 0 0 0-.76.063L8 11 5.835 9.7a.5.5 0 0 0-.611.076L3 12V2z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M6.502 7a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
+            />
+          </svg>
+          {{ fileText }}
+        </label>
+        <div
+          class="button convert"
+          v-if="fileText !== 'Feed me images...'"
+          @click="onFileUpload()"
+        >
+          Convert!
+          <div v-if="converting" class="loader"></div>
+        </div>
       </div>
+      <div class="button subpixel-sw" @click="subPixelSwitch()">
+        <span class="material-icons">{{
+          checked ? "check_box" : "check_box_outline_blank"
+        }}</span>
+        Include Subpixels
+      </div>
+      <!-- <input type="checkbox" class="toggle-switch oversize" id="toggle3">
+        <label for="toggle3">Oversized knob</label> -->
+      <!-- <input type="checkbox" id="checkbox" class="toggle-switch oversize" v-model="checked">
+        <label for="checkbox">Include Subpixels</label> -->
     </div>
-    <div class="credit">Made by <a href="https://joshlucpoll.com" target="_blank" rel="noopener noreferrer">Joshlucpoll</a></div>
+    <div class="credit">
+      Made by
+      <a
+        href="https://joshlucpoll.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Joshlucpoll</a
+      >
+    </div>
   </div>
 </template>
-
 
 <script>
 import Jimp from "jimp";
 import Excel from "exceljs";
 
 export default {
-  name: 'App',
+  name: "App",
 
   data() {
     return {
       fileText: "Feed me images...",
       converting: false,
-      workbookURL: false,
-    }
+      workbookURL: { whole: false, sub: false },
+      checked: false,
+    };
   },
   methods: {
-
     toColumnName(num) {
-      for (var ret = '', a = 1, b = 26; (num -= a) >= 0; a = b, b *= 26) {
+      for (var ret = "", a = 1, b = 26; (num -= a) >= 0; a = b, b *= 26) {
         ret = String.fromCharCode(parseInt((num % b) / a) + 65) + ret;
       }
       return ret;
     },
 
-
     onFileChange(event) {
       this.file = event.target.files[0];
       this.fileText = this.file.name;
-      this.fileName = this.file.name.split('.').slice(0, -1).join('.');
-      this.workbookURL = false;
+      this.fileName = this.file.name
+        .split(".")
+        .slice(0, -1)
+        .join(".");
+      this.workbookURL = { whole: false, sub: false };
 
       if (this.file) {
         this.displayPreview();
       }
     },
 
+    subPixelSwitch() {
+      this.checked = !this.checked;
+    },
 
     async downloadFile(url) {
-
-      const download = document.createElement('a');
+      const download = document.createElement("a");
       download.setAttribute("href", url);
       download.setAttribute("download", `${this.fileName}-to-spreadsheet.xlsx`);
 
-      download.style.display = 'none';
+      download.style.display = "none";
       document.body.appendChild(download);
 
       download.click();
@@ -70,113 +116,156 @@ export default {
       document.body.removeChild(download);
     },
 
-
     sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
+      return new Promise((resolve) => setTimeout(resolve, ms));
     },
 
+    convert() {
+      var reader = new FileReader();
+      reader.onload = (e) => {
+        this.converting = true;
 
-    onFileUpload() {
-      if (this.workbookURL) {
-        this.downloadFile(this.workbookURL);
-      }
-      else {
-        var reader = new FileReader();
-        reader.onload = (e) => {
-          this.converting = true;
-  
-          Jimp.read(e.target.result)
+        Jimp.read(e.target.result)
           .then(async (image) => {
-
-
-            image.scaleToFit(400, 600);
+            image.scaleToFit(400, this.checked ? 200 : 600);
             const pixelData = [];
-  
-            image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
-            
+
+            image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(
+              x,
+              y,
+              idx
+            ) {
               var red = this.bitmap.data[idx + 0];
               var green = this.bitmap.data[idx + 1];
               var blue = this.bitmap.data[idx + 2];
-  
+
               pixelData.push([red, green, blue]);
             });
-  
+
             const workbook = new Excel.Workbook();
-            const sheet = workbook.addWorksheet('Image', {properties: {defaultColWidth: 3}});
-  
+            const sheet = workbook.addWorksheet("Image", {
+              properties: { defaultColWidth: this.checked ? 10 : 3 },
+            });
+
+            const componentToHex = (c) => {
+              var hex = c.toString(16);
+              return hex.length == 1 ? "0" + hex : hex;
+            };
+
             let count = 0;
-            
-            for (let y = 0; y < image.bitmap.height; y++) {
-              for (let x = 0; x < image.bitmap.width; x++) {
-                const cellNum = this.toColumnName(x + 1) + (y + 1).toString();
-                const cell = sheet.getCell(cellNum);
 
-                const componentToHex = (c) => {
-                  var hex = c.toString(16);
-                  return hex.length == 1 ? "0" + hex : hex;
+            if (this.checked) {
+              for (let row = 0; row < image.bitmap.height; row++) {
+                for (let col = 0; col < image.bitmap.width; col++) {
+                  for (let subpixel = 0; subpixel < 3; subpixel++) {
+                    const cellNum =
+                      this.toColumnName(col + 1) +
+                      ((row + 1) * 3 - 2 + subpixel);
+                    const cell = sheet.getCell(cellNum);
+
+                    let colour = "00";
+                    if (subpixel == 0) {
+                      colour += componentToHex(pixelData[count][0]) + "0000";
+                    }
+                    if (subpixel == 1) {
+                      colour +=
+                        "00" + componentToHex(pixelData[count][1]) + "00";
+                    }
+                    if (subpixel == 2) {
+                      colour += "0000" + componentToHex(pixelData[count][2]);
+                    }
+
+                    cell.fill = {
+                      type: "pattern",
+                      pattern: "solid",
+                      fgColor: { argb: colour },
+                    };
+                  }
+                  count++;
                 }
-
-                let colour = '00' +
-                  componentToHex(pixelData[count][0]) +
-                  componentToHex(pixelData[count][1]) +
-                  componentToHex(pixelData[count][2]);
-
-                cell.fill = {
-                  type: "pattern",
-                  pattern: "solid",
-                  fgColor:{argb: colour}
-                };
-
-                count++;
               }
-              await this.sleep(1);
+            } else {
+              for (let y = 0; y < image.bitmap.height; y++) {
+                for (let x = 0; x < image.bitmap.width; x++) {
+                  const cellNum = this.toColumnName(x + 1) + (y + 1).toString();
+                  const cell = sheet.getCell(cellNum);
+
+                  let colour =
+                    "00" +
+                    componentToHex(pixelData[count][0]) +
+                    componentToHex(pixelData[count][1]) +
+                    componentToHex(pixelData[count][2]);
+
+                  cell.fill = {
+                    type: "pattern",
+                    pattern: "solid",
+                    fgColor: { argb: colour },
+                  };
+
+                  count++;
+                }
+                await this.sleep(1);
+              }
             }
 
-            const buffer = await workbook.xlsx.writeBuffer()
-            const blob = new Blob([buffer], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+            const buffer = await workbook.xlsx.writeBuffer();
+            const blob = new Blob([buffer], {
+              type:
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            });
             const url = window.URL.createObjectURL(blob);
 
-            this.workbookURL = url;
+            if (this.checked) {
+              this.workbookURL.sub = url
+            }
+            else {
+              this.workbookURL.whole = url
+            }
             this.converting = false;
 
             this.downloadFile(url);
           })
-          .catch(err => {
+          .catch((err) => {
             throw Error(err);
-          })
-        };
-  
-        reader.readAsArrayBuffer(this.file);
+          });
+      };
+
+      reader.readAsArrayBuffer(this.file);
+    },
+
+    onFileUpload() {
+      if (!this.checked && this.workbookURL.whole) {
+        this.downloadFile(this.workbookURL.whole);
+      } else if (this.checked && this.workbookURL.sub) {
+        this.downloadFile(this.workbookURL.sub);
+      } else {
+        this.convert();
       }
     },
 
-
     displayPreview() {
       var reader = new FileReader();
-    
-      reader.onload = (e) => {
 
+      reader.onload = (e) => {
         const img = document.body.contains(document.getElementById("preview"))
-        ? document.getElementById("preview")
-        : document.createElement('img');
+          ? document.getElementById("preview")
+          : document.createElement("img");
 
         img.id = "preview";
         img.setAttribute("src", e.target.result);
 
         document.getElementById("file-input-container").before(img);
-      }
-      
+      };
+
       reader.readAsDataURL(this.file);
     },
-  }
-}
+  },
+};
 </script>
-
-
 
 <style>
 body {
-  font-family: 'Kumbh Sans', sans-serif;
+  font-family: "Kumbh Sans", sans-serif;
   width: 100%;
   height: 100%;
   margin: 0;
@@ -206,6 +295,14 @@ body {
   font-size: 40px;
 }
 
+.col {
+  /* height: 20vh; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+
 #file {
   width: 0.1px;
   height: 0.1px;
@@ -222,25 +319,28 @@ body {
 }
 
 .file-label {
-  margin-right: 20px;
+  margin: 0 20px;
 }
 
 .button {
   padding: 15px;
   padding-bottom: 10px;
   font-size: 22px;
-  text-align: center;
+  /* text-align: center; */
   text-decoration: none;
   color: white;
   text-shadow: 0 -1px -1px #0f864a;
   cursor: pointer;
 
-  background-color: #313D53;
+  background-color: #313d53;
   border-radius: 4px;
   box-shadow: 0 4px 0 rgba(31, 39, 53, 0.75), 0 5px 5px 1px rgba(0, 0, 0, 0.4);
 
   transition: all 0.15s ease-in-out;
   user-select: none;
+
+  /* display: flex;
+  align-items: stretch; */
 }
 
 .button:hover {
@@ -252,7 +352,12 @@ body {
 .button:active {
   margin-bottom: -4px;
   box-shadow: none;
-} 
+}
+
+.material-icons {
+  /* margin-right: 5px; */
+  transform: translateY(3px);
+}
 
 #file-icon {
   height: 18px;
@@ -260,8 +365,8 @@ body {
   transform: translateY(1px);
 }
 
-
 #preview {
+  margin-bottom: 3vw;
   height: 20vh;
   position: relative;
   border: black solid 2px;
@@ -284,9 +389,12 @@ body {
   animation: spin 2s linear infinite;
 }
 
+.subpixel-sw {
+  margin-top: 5vw;
+}
 
 .credit {
-  background-color: #313D53;
+  background-color: #313d53;
   color: rgba(256, 256, 256, 0.4);
   padding: 10px;
   position: absolute;
@@ -325,15 +433,22 @@ body {
   transform: scaleX(1);
 }
 
-
 @-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
 }
 
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
 /* Copyright (c) 2020 by Manuel Pinto (https://codepen.io/P1N2O/pen/pyBNzX)
@@ -350,15 +465,14 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 }
 
 @keyframes gradient {
-    0% {
-        background-position: 0% 0%;
-    }
-    50% {
-        background-position: 100% 100%;
-    }
-    100% {
-        background-position: 0% 0%;
-    }
+  0% {
+    background-position: 0% 0%;
+  }
+  50% {
+    background-position: 100% 100%;
+  }
+  100% {
+    background-position: 0% 0%;
+  }
 }
-
 </style>
